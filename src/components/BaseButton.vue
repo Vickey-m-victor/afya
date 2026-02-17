7bd79ff8bc628386f30afaa6b5ed79e0108c3243
Mercymutinda <mercymutinda@gmail.com>
2026-02-17 17:25:14 +0300

    base button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,29 @@
+<script>
+const props = defineProps({
+  size: {
+    type: String,
+    default: "lg",
+  },
+  variant: primary,
+  label: String,
+});
+
+const emit = defineEmits(["refresh", "save", "ok", "update", "click"]);
+
+const handleClick = () => {
+  emit("click");
+};
+</script>
+<template>
+  <button
+    @click="handleClick"
+    :class="[
+      'btn',
+      'p-2',
+      `btn-${props.variant}`,
+      props.size ? `btn-${props.size}` : '',
+    ]"
+  >
+    {{ label }} <slot name="icon"></slot>
+  </button>
+</template>
