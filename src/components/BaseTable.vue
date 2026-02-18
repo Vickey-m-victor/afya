369e85bd071981fb82be531bdd25425ebbadeaa3
Mercymutinda <mercymutinda@gmail.com>
2026-02-18 14:28:54 +0300

    base table

--- src/components/BaseTable.vue
+++ src/components/BaseTable.vue
@@ -1,80 +1,81 @@
 <script setup>
+const props = defineProps({
+  data: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+  columns: {
+    type: Array,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+  },
+  searchFields: {
+    type: Array,
+    default: () => [],
+  },
+});
+const emit = defineEmits(["click", "close", "refresh"]);
 
-const props = defineProps(
-    {
-        
-    }
-);
-const emit = defineEmits(
-  [
-    "save", "click", "close", "ok", "refresh"
-  ]
-);
+const onSort = (event, index) => {};
 
-const handleClick = ()  => {
-    emit( "click")
-}
-
+const handleClick = () => {
+  emit("click");
+};
 </script>
 
 <template>
-    
-    <div class="content">
-    <BaseBlock title="Vue Dataset" content-full>
-      <Dataset
-        v-slot="{ ds }"
-        :ds-data="users"
-        :ds-sortby="sortBy"
-        :ds-search-in="['name', 'email', 'company', 'birthdate']"
-      >
-        <div class="row" :data-page-count="ds.dsPagecount">
-          <div id="datasetLength" class="col-md-8 py-2">
-            <DatasetShow />
-          </div>
-          <div class="col-md-4 py-2">
-            <DatasetSearch ds-search-placeholder="Search..." />
-          </div>
-        </div>
-        <hr />
-        <div class="row">
-          <div class="col-md-12">
-            <div class="table-responsive">
-              <table class="table table-striped mb-0">
-                <thead>
-                  <tr>
-                    <th scope="col">ID</th>
-                    <th
-                      v-for="(th, index) in cols"
-                      :key="th.field"
-                      :class="['sort', th.sort]"
-                      @click="onSort($event, index)"
-                    >
-                      {{ th.name }} <i class="gg-select float-end"></i>
-                    </th>
-                  </tr>
-                </thead>
-                <DatasetItem tag="tbody" class="fs-sm">
-                  <template #default="{ row, rowIndex }">
-                    <tr>
-                      <th scope="row">{{ rowIndex + 1 }}</th>
-                      <td style="min-width: 150px">{{ row.name }}</td>
-                      <td>{{ row.email }}</td>
-                      <td style="min-width: 150px">{{ row.company }}</td>
-                      <td style="min-width: 150px">{{ row.birthdate }}</td>
-                    </tr>
-                  </template>
-                </DatasetItem>
-              </table>
-            </div>
-          </div>
-        </div>
-        <div
-          class="d-flex flex-md-row flex-column justify-content-between align-items-center"
-        >
-          <DatasetInfo class="py-3 fs-sm" />
-          <DatasetPager class="flex-wrap py-3 fs-sm" />
-        </div>
-      </Dataset>
-    </BaseBlock>
-  </div>
+  <tbody v-if="loading">
+    <tr v-for="n in 5" :key="n">
+      <td v-for="col in columns.length + 1" :key="col">
+        <div class="shimmer"></div>
+      </td>
+    </tr>
+  </tbody>
+  <DatasetItem v-else tag="tbody" :ds="data" class="fs-sm">
+    <template #default="{ row, rowIndex }">
+      <tr>
+        <th scope="row">{{ rowIndex + 1 }}</th>
+        <td v-for="col in columns" :key="col.field">
+          <slot :name="`cell(${col.field})`" :row="row">
+            {{ row[col.field] }}
+          </slot>
+        </td>
+      </tr>
+    </template>
+  </DatasetItem>
 </template>
+<style scoped>
+.shimmer {
+  height: 12px;
+  background: #f6f7f8;
+  background-image: linear-gradient(
+    to right,
+    #f6f7f8 0%,
+    #edeef1 20%,
+    #f6f7f8 40%,
+    #f6f7f8 100%
+  );
+  background-repeat: no-repeat;
+  background-size: 800px 100%;
+  display: inline-block;
+  position: relative;
+  width: 100%;
+  animation: shimmer 1.5s linear infinite forwards;
+}
+
+@keyframes shimmer {
+  0% {
+    background-position: -468px 0;
+  }
+  100% {
+    background-position: 468px 0;
+  }
+}
+</style>
--- src/views/Tests/ComponentsTest.vue
+++ src/views/Tests/ComponentsTest.vue
@@ -1,7 +1,20 @@
 <script setup>
-
+import { ref } from 'vue';
+import BaseTable from '@/components/BaseTable.vue';
 </script>
 
 <template>
 
+<BaseTable 
+  title="Patient List"
+  :data="patients" 
+  :columns="[{name: 'Name', field: 'name'}, {name: 'Actions', field: 'actions'}]"
+  :loading="isFetching"
+>
+  <template #cell(actions)="{ row }">
+    <button class="btn btn-sm btn-alt-primary" @click="edit(row.id)">
+      <i class="fa fa-pencil-alt"></i>
+    </button>
+  </template>
+</BaseTable>
 </template>
